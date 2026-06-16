44e8df2b47bc405d9b0cd88ee4dac2cb9df28eaf
AIESIT <support@arava.org>
2026-06-16 13:01:04 +0300

    Update 0093_add_bm25_indexes_with_icu.sql

--- packages/database/migrations/0093_add_bm25_indexes_with_icu.sql
+++ packages/database/migrations/0093_add_bm25_indexes_with_icu.sql
@@ -5,8 +5,8 @@
 
 -- 1. agents: title, description, slug, tags(jsonb), system_role, user_id
 DROP INDEX IF EXISTS agents_bm25_idx;--> statement-breakpoint
-CREATE INDEX agents_bm25_idx ON agents
-USING bm25 (id, title, description, slug, tags, system_role, user_id)
+--CREATE INDEX agents_bm25_idx ON agents
+--USING bm25 (id, title, description, slug, tags, system_role, user_id)
 WITH (
   key_field = 'id',
   text_fields = '{
